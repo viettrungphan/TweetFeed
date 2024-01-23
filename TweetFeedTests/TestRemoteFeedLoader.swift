08c5c7eed7ec2a4004ee5e74dbb22868f9e6d9a5
Trung <viettrungphan90@gmail.com>
2024-01-23 21:34:12 +0700

    Test load feed success

--- TweetFeedTests/TestRemoteFeedLoader.swift
+++ TweetFeedTests/TestRemoteFeedLoader.swift
@@ -7,13 +7,25 @@
 
 import XCTest
 
+struct FeedItem {
+}
+
 final class RemoteFeedLoader {
-    
+    func fetchFeed() -> [FeedItem] {
+        return []
+    }
 }
 
 final class TestRemoteFeedLoader: XCTestCase {
     func test_Init_RemoteFeedLoader_Success() {
         let feedLoader = RemoteFeedLoader()
         XCTAssertNotNil(feedLoader)
+    }
+    
+    func test_fetchFeed_Success() {
+        let feedLoader = RemoteFeedLoader()
+        
+        let feed = feedLoader.fetchFeed()
+        XCTAssertNotNil(feed)
     }
 }
