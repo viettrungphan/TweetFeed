e37544d429064b4ad51a23431f1adb1872f3b97b
Trung <viettrungphan90@gmail.com>
2024-01-23 21:53:22 +0700

    Encapsulated the success and failed into Result type, test load feed failed

--- TweetFeedTests/TestRemoteFeedLoader.swift
+++ TweetFeedTests/TestRemoteFeedLoader.swift
@@ -11,9 +11,22 @@ struct FeedItem {
 }
 
 final class RemoteFeedLoader {
-    func fetchFeed(onSuccess: ([FeedItem])->Void) {
-        onSuccess([])
+    var isSuccess = false
+    func fetchFeed(onComplete: (Result<[FeedItem], Error>)->Void) {
+        if isSuccess {
+            onComplete(.success([]))
+        } else {
+            onComplete(.failure(self.anyError()))
+        }
     }
+    
+    func anyError() -> Error {
+        Error.any
+    }
+}
+
+enum Error: Swift.Error {
+    case any
 }
 
 final class TestRemoteFeedLoader: XCTestCase {
@@ -24,13 +37,30 @@ final class TestRemoteFeedLoader: XCTestCase {
     
     func test_fetchFeed_Success() {
         let feedLoader = self.makeSUT()
-        
+        feedLoader.isSuccess = true
         let exp = self.expectation(description: "Expect load feed success")
         
-        feedLoader.fetchFeed(onSuccess: { feed in
+        feedLoader.fetchFeed(onComplete: { feed in
             XCTAssertNotNil(feed)
             exp.fulfill()
-        })        
+        })
+        self.wait(for: [exp], timeout: 1)
+    }
+    
+    func test_fetchFeed_Failed() {
+        let feedLoader = self.makeSUT()
+        feedLoader.isSuccess = false
+        let exp = self.expectation(description: "Expect load feed failed")
+        
+        feedLoader.fetchFeed(onComplete: { feed in
+            switch feed {
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                exp.fulfill()
+            case .success(_):
+                assert(true, "Should not fall here")
+            }
+        })
         self.wait(for: [exp], timeout: 1)
     }
 }
