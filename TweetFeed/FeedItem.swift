509e96a0763b208469c1237a9e315d688687bba5
Trung <viettrungphan90@gmail.com>
2024-01-24 15:08:54 +0700

    Test local storage had cache data, return cache data

--- TweetFeed/FeedItem.swift
+++ TweetFeed/FeedItem.swift
@@ -8,4 +8,5 @@
 import Foundation
 
 struct FeedItem: Decodable {
+    let id: UUID
 }
--- TweetFeedTests/TestLocalFeedLoader.swift
+++ TweetFeedTests/TestLocalFeedLoader.swift
@@ -8,8 +8,10 @@
 import XCTest
 
 final class LocalFeedLoader: FeedLoader {
+    var localData:[FeedItem] = []
+    
     func fetchFeed(onComplete: @escaping (Result<[FeedItem], Error>) -> Void) {
-        onComplete(.success([]))
+        onComplete(.success(localData))
     }
 }
 
@@ -24,7 +26,6 @@ final class TestLocalFeedLoader: XCTestCase {
         let feedLoader = self.makeSUT()
         
         let exp = self.expectation(description: "Expect Init new LocalFeedLoader should return an Empty feed")
-        
         feedLoader.fetchFeed { result in
             switch result {
             case .success(let feed):
@@ -37,6 +38,27 @@ final class TestLocalFeedLoader: XCTestCase {
         }
         self.wait(for: [exp], timeout: 1)
     }
+    
+    func test_Storage_Has_Cache_ReturnCache() {
+        let feedLoader = self.makeSUT()
+        
+        let exp = self.expectation(description: "Expect Local Storage had cache data, return cache data")
+        
+        let mockLocalData = self.mockLocalData()
+        feedLoader.localData = mockLocalData
+        
+        feedLoader.fetchFeed { result in
+            switch result {
+            case .success(let feed):
+                XCTAssertEqual(feed, mockLocalData)
+                exp.fulfill()
+            case .failure(_):
+                assert(true, "Should not fall here")
+            }
+        }
+        self.wait(for: [exp], timeout: 1)
+
+    }
 }
 
 extension TestLocalFeedLoader {
@@ -44,5 +66,20 @@ extension TestLocalFeedLoader {
         let feedLoader = LocalFeedLoader()
         trackForMemoryLeaks(feedLoader)
         return feedLoader
+    }
+    
+    func anyFeedItem() -> FeedItem {
+        FeedItem(id: UUID())
+    }
+    
+    func mockLocalData() -> [FeedItem] {
+        [anyFeedItem()]
+    }
+    
+}
+
+extension FeedItem: Equatable {
+    static func == (lhs: FeedItem, rhs: FeedItem) -> Bool {
+        lhs.id == rhs.id
     }
 }
