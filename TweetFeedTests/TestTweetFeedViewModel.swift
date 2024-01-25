24d87e03f4bcafa16a91677d1e2f2bae386990df
Trung <viettrungphan90@gmail.com>
2024-01-25 09:50:29 +0700

    Test fetchFeed success

--- TweetFeedTests/TestTweetFeedViewModel.swift
+++ TweetFeedTests/TestTweetFeedViewModel.swift
@@ -8,12 +8,27 @@
 import XCTest
 
 final class TweetFeedViewModel {
-    
+    func loadFeed(onSuccess: ([FeedItem]) -> Void) {
+        onSuccess([])
+    }
 }
 
 final class TestTweetFeedViewModel: XCTestCase {
     func test_Init_Should_Success() {
         let viewModel = TweetFeedViewModel()
         XCTAssertNotNil(viewModel)
+    }
+    
+    func test_LoadFeed_Success() {
+        let viewModel = TweetFeedViewModel()
+        
+        let exp = self.expectation(description: "Expect load feed success")
+        
+        viewModel.loadFeed(onSuccess:{ feed in
+            XCTAssertNotNil(feed)
+            exp.fulfill()
+        })
+        
+        self.wait(for: [exp], timeout: 1)
     }
 }
