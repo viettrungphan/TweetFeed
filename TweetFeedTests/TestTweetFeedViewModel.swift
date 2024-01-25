85793e94edde92b015b5a7cc6e0918852a5c4a27
Trung <viettrungphan90@gmail.com>
2024-01-25 09:57:51 +0700

    Test load feed failure case

--- TweetFeedTests/TestTweetFeedViewModel.swift
+++ TweetFeedTests/TestTweetFeedViewModel.swift
@@ -7,9 +7,18 @@
 
 import XCTest
 
+enum TweetError: Error {
+    case any
+}
+
 final class TweetFeedViewModel {
-    func loadFeed(onSuccess: ([FeedItem]) -> Void) {
-        onSuccess([])
+    var mockIsSuccess = false
+    func loadFeed(onComplete: (Result<[FeedItem], Error>) -> Void) {
+        if mockIsSuccess {
+            onComplete(.success([]))
+        } else {
+            onComplete(.failure(TweetError.any))
+        }
     }
 }
 
@@ -21,11 +30,35 @@ final class TestTweetFeedViewModel: XCTestCase {
     
     func test_LoadFeed_Success() {
         let viewModel = TweetFeedViewModel()
-        
+        viewModel.mockIsSuccess = true
         let exp = self.expectation(description: "Expect load feed success")
         
-        viewModel.loadFeed(onSuccess:{ feed in
-            XCTAssertNotNil(feed)
+        viewModel.loadFeed(onComplete:{ result in
+            switch result {
+            case .success(let feed):
+                XCTAssertNotNil(feed)
+                exp.fulfill()
+            case .failure(_):
+                assert(true, "Should not fall here")
+            }
+            
+        })
+        
+        self.wait(for: [exp], timeout: 1)
+    }
+    
+    func test_LoadFeed_Failed() {
+        let viewModel = TweetFeedViewModel()
+        viewModel.mockIsSuccess = false
+        let exp = self.expectation(description: "Expect load feed failed")
+        
+        viewModel.loadFeed(onComplete:{ result in
+            switch result {
+            case .failure(let error):
+                XCTAssertNotNil(error)
+            case .success(_):
+                assert(true, "Should not fall here")
+            }
             exp.fulfill()
         })
         
