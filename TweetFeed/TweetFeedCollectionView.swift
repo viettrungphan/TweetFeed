78bcc9c567667cfa2f5a13b7d19000aa21ad38bb
Trung <viettrungphan90@gmail.com>
2024-01-26 12:40:15 +0700

    Add TweetFeedCollectionView

---
+++ TweetFeed/TweetFeedCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  TweetFeedCollectionView.swift
+//  TweetFeed
+//
+//  Created by MacOS on 26/01/2024.
+//
+
+import UIKit
+
+class TweetFeedCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
